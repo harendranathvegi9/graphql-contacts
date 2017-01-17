75eaed59e587d03f5779a82d01afc5a5cb7a05ff
dchambers <dominic.chambers@gmail.com>
2017-01-17 16:52:25 +0000

    Add missing form validation

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -3,27 +3,34 @@ import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 import { TextField, DatePicker } from 'redux-form-material-ui';
 import RaisedButton from 'material-ui/RaisedButton';
+import isEmail from 'validator/lib/isEmail';
 import style from './style.css';
 
 const required = value => value ? undefined : 'Required';
+const identifier = value => (value && !value.match(/^[a-zA-Z0-9-]+$/)) ?
+  'Id can contain only alphanumeric characters and dashes (-)' : undefined;
+const email = value => (value && !isEmail(value)) ? 'Invalid email' : undefined;
+const street = value => (value && !value.match(/^[0-9]+ .+$/)) ?
+  'The street must be a number followed by a name' : undefined;
 
 const ContactForm = ({id, handleSubmit, saveContact, reset}) => {
   return (
     <form onSubmit={handleSubmit(saveContact)}>
       <Field component={TextField} floatingLabelText="Contact Id" name="id"
-        hintText="Unique identifier" fullWidth={true} validate={[required]}
-        disabled={id !== undefined}/><br/>
+        hintText="Unique identifier" fullWidth={true}
+        validate={[required, identifier]} disabled={id !== undefined}/><br/>
       <Field component={TextField} floatingLabelText="Name" name="name"
         validate={[required]} hintText="Full name" fullWidth={true}/><br/>
       <Field component={TextField} floatingLabelText="Company" name="company"
         hintText="Company name" fullWidth={true}/><br/>
       <Field component={TextField} floatingLabelText="Email" name="email"
-        hintText="Email address" fullWidth={true}/><br/>
+        hintText="Email address" fullWidth={true} validate={[email]}/><br/>
       <Field component={TextField} floatingLabelText="Phone" name="phone"
         hintText="Primary phone number" fullWidth={true}/><br/>
       <div className={style.addressFields}>
         <Field component={TextField} floatingLabelText="Street" name="street"
-          hintText="Street name" fullWidth={true} style={{flex: 4}}/><br/>
+          hintText="Street name" fullWidth={true} style={{flex: 4}}
+          validate={[street]}/><br/>
         <Field component={TextField} floatingLabelText="City" name="city"
           hintText="City or town" fullWidth={true} style={{flex: 2}}
           validate={[required]}/><br/>
